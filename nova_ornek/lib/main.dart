c3c728e270e274cf2223fa9490d061185250cb60
Aslınur Topcu <Asl.nurtopcu13@gmail.com>
2024-02-13 19:32:13 +0300

    final

--- nova_ornek/lib/main.dart
+++ nova_ornek/lib/main.dart
@@ -11,27 +11,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Digi Nova',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(), // Giriş sayfasını burada belirtin
+    );
+  }
+
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Digi Nova Giriş'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Kullanıcı Adı'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Şifre'),
+              obscureText: true,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Giriş butonuna tıklanınca yapılacak işlemler
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()), // Home sayfasını burada belirtin
+                );
+              },
+              child: Text('Giriş Yap'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -52,6 +73,20 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Digi Nova Home'),
+      ),
+      body: Center(
+        child: Text('Hoş Geldiniz!'),
+      ),
+    );
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
